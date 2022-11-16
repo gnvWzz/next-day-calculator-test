19ec2f9cb001bb0f5f7c2d20311128742dc84362
gnvWzz <quangvuongdac2111@gmail.com>
2022-11-16 14:29:18 +0700

    14:29.16/11/2022

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -1,16 +1,8 @@
 public class NextDayCalculator {
-
     public static String displayNextDate(int day, int month, int year) {
-        boolean isLeapYear = checkLeapYear(year);
-
-        if (isLeapYear) {
-            return nextDateInLeapYear(day, month, year);
-        } else {
-            return nextDayInCommonYear(day, month, year);
-        }
+        return nextDate(day,month,year);
     }
-
-    private static String nextDayInCommonYear(int day, int month, int year) {
+    private static String nextDate(int day, int month, int year) {
         switch (month) {
         case 1:
         case 3:
@@ -18,20 +10,16 @@ public class NextDayCalculator {
         case 7:
         case 8:
         case 10:
-            if (day < 31 && day >= 1) {
-                day += 1;
-            } else if (day == 31) {
-                day -= 30;
-                month += 1;
-            }
-            break;
         case 12:
-            if (day < 31 && day >= 1) {
-                day += 1;
-            } else if (day == 31) {
+            if (day < 31 && day >= 1 && month != 12) {
+                day++;
+            } else if (day == 31 && month == 12) {
                 day -= 30;
                 month -= 11;
-                year += 1;
+                year++;
+            } else {
+                day = 1;
+                month++;
             }
             break;
         case 4:
@@ -46,69 +34,32 @@ public class NextDayCalculator {
             }
             break;
         case 2:
-            if (day < 28 && day >= 1) {
-                day += 1;
-            } else if (day == 28) {
-                day -= 27;
-                month += 1;
+            if (day < 28) {
+                day++;
+            } else {
+                if (checkLeapYear(year)) {
+                    if (day == 28) {
+                        day++;
+                    } else if (day == 29) {
+                        day = 1;
+                        month++;
+                    }
+                } else {
+                    day = 1;
+                    month++;
+                }
             }
-            break;
         }
         return day + "/" + month + "/" + year;
     }
-
-    private static String nextDateInLeapYear(int day, int month, int year) {
-        switch (month) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-                if (day < 31 && day >= 1) {
-                    day += 1;
-                } else if (day == 31) {
-                    day -= 30;
-                    month += 1;
-                }
-                break;
-            case 12:
-                if (day < 31 && day >= 1) {
-                    day += 1;
-                } else if (day == 31) {
-                    day -= 30;
-                    month -= 11;
-                    year += 1;
-                }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                if (day < 30 && day >= 1) {
-                    day += 1;
-                } else if (day == 30) {
-                    day -= 29;
-                    month += 1;
-                }
-                break;
-            case 2:
-                if (day < 29 && day >= 1) {
-                    day += 1;
-                } else if (day == 29) {
-                    day -= 28;
-                    month += 1;
-                }
-                break;
-        }
-        return day + "/" + month + "/" + year;
-    }
-
     private static boolean checkLeapYear(int year) {
         boolean isLeapYear = false;
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0) {
+        boolean isDivisibleBy4 = year % 4 == 0;
+        boolean isDivisibleBy100 = year % 100 == 0;
+        boolean isDivisibleBy400 = year % 400 == 0;
+        if (isDivisibleBy4) {
+            if (isDivisibleBy100) {
+                if (isDivisibleBy400) {
                     isLeapYear = true;
                 }
             } else {
